600a16d70cda63aeb56db092535f5bff886a8dbd
miso-devel <kengo279@icloud.com>
2023-03-28 23:57:19 +0900

    cats

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('cats')
+  findAll(): string {
+    return 'This action returns all catsAAA';
+  }
 }
